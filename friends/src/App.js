612ad254fca8c5cf73cbab235c6e72b147154a43
aaronjan <aaronjan98@gmail.com>
2020-01-28 12:46:27 -0800

    wrapped App w/ Router

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Route, Link, Switch } from 'react-router-dom';
+
 
 import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World!</h1>
-     <Login  />
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          {/* <PrivateRoute path="/protected" component={GasPrices} /> */}
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
     </div>
   );
 }
