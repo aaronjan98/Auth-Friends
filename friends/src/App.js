1e32016b85005cde577017d4bdfde2e46368d4ad
aaronjan <aaronjan98@gmail.com>
2020-01-28 12:38:01 -0800

    created privateRoute component
    & the utils folder and axiosWithAuth.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Login from './components/Login';
+
 function App() {
   return (
     <div className="App">
      <h1>Hello World!</h1>
+     <Login  />
     </div>
   );
 }
---
+++ friends/src/components/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import axios from 'axios';
+
+const Login = () => {
+    return <h4>Hi from Login</h4>
+}
+
+export default Login;
